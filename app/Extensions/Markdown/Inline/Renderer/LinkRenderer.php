268c83d39b3b1fe06df4e6df6dea05653b330702
Nathan Soares <eu@nathan.com.br>
2019-05-30 23:18:41 -0300

    dev

--- app/Extensions/Markdown/Inline/Renderer/LinkRenderer.php
+++ app/Extensions/Markdown/Inline/Renderer/LinkRenderer.php
@@ -3,21 +3,46 @@
 
 namespace App\Extensions\Markdown\Inline\Renderer;
 
-
+use League\CommonMark\ElementRendererInterface;
+use League\CommonMark\HtmlElement;
+use League\CommonMark\Inline\Element\AbstractInline;
+use League\CommonMark\Inline\Element\Link;
+use League\CommonMark\Util\ConfigurationAwareInterface;
+use League\CommonMark\Util\ConfigurationInterface;
+use League\CommonMark\Util\RegexHelper;
 use League\CommonMark\Inline\Renderer\LinkRenderer as Render;
 
 class LinkRenderer extends Render
 {
 
     /**
-     * @param Center $block
+     * @param Link                     $inline
      * @param ElementRendererInterface $htmlRenderer
-     * @param bool $inTightList
      *
      * @return HtmlElement
      */
-    public function render(AbstractBlock $block, ElementRendererInterface $htmlRenderer, $inTightList = false)
+    public function render(AbstractInline $inline, ElementRendererInterface $htmlRenderer)
     {
+        if (!($inline instanceof Link)) {
+            throw new \InvalidArgumentException('Incompatible inline type: ' . \get_class($inline));
+        }
 
+        $attrs = $inline->getData('attributes', []);
+
+        $forbidUnsafeLinks = !$this->config->get('allow_unsafe_links');
+
+        if (!($forbidUnsafeLinks && RegexHelper::isLinkPotentiallyUnsafe($inline->getUrl()))) {
+            $attrs['href'] = $inline->getUrl();
+        }
+
+        if (isset($inline->data['title'])) {
+            $attrs['title'] = $inline->data['title'];
+        }
+
+        if (url_to_domain($inline->getUrl()) != url_to_domain(env('APP_URL'))) {
+            $attrs['rel'] = 'nofollow';
+        }
+
+        return new HtmlElement('a', $attrs, $htmlRenderer->renderInlines($inline->children()));
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -126,3 +126,23 @@ if (!function_exists('plural')) {
         return $amount == 1 ? "${amount} ${singular}" : ($amount == 0 && $zero ? "${zero}" : "${amount} ${plural}");
     }
 }
+
+
+if (!function_exists('url_to_domain')) {
+    function url_to_domain($url)
+    {
+        $host = @parse_url($url, PHP_URL_HOST);
+        // If the URL can't be parsed, use the original URL
+        // Change to "return false" if you don't want that
+        if (!$host)
+            $host = $url;
+        // The "www." prefix isn't really needed if you're just using
+        // this to display the domain to the user
+        if (substr($host, 0, 4) == "www.")
+            $host = substr($host, 4);
+        // You might also want to limit the length if screen space is limited
+        if (strlen($host) > 50)
+            $host = substr($host, 0, 47) . '...';
+        return $host;
+    }
+}
