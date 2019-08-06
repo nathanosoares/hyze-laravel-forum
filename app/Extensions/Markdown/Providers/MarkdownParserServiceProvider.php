57604296a5bced053f5028e1dda6bbcd6b62ddc3
Nathan Soares <eu@nathan.com.br>
2019-08-06 20:49:21 -0300

    fix: common mark configg

--- app/Extensions/Markdown/Providers/MarkdownParserServiceProvider.php
+++ app/Extensions/Markdown/Providers/MarkdownParserServiceProvider.php
@@ -48,13 +48,16 @@ class MarkdownParserServiceProvider extends ServiceProvider
                 'html_input' => 'escape',
                 'allow_unsafe_links' => false,
                 'max_nesting_level' => '5',
-                'internal_hosts' => url_to_domain(env('APP_URL')),
                 'open_in_new_window' => true,
                 'html_class' => 'external-link',
                 'renderer' => [
                     'block_separator' => "\n",
                     'inner_separator' => "\n",
                     'soft_break'      => "<br>",
+                ],
+                'external_link' => [
+                    'internal_hosts' => url_to_domain(env('APP_URL')),
+                    'open_in_new_window' => true,
                 ]
             ], $environment);
 
